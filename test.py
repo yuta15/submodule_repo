a6d1e0460a8f8cd89a195b8528fdcde41c963386
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-04-22 19:56:29 +0900

    submodule update

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import csv
 
 def main():
     file_path = '~/test.csv'
+    print(os.getcwd())
     with open(file_path, mode='w') as f:
         writer = csv.writer(f)
         header = ['username', 'password']
         writer.writerow(header)
         writer.writerow(['testuser', 'testpass'])
-    
     
     
 if __name__ == '__main__':
